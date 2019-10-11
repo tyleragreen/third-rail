0b80a6c9c37538728dc6fda6e722d742f1560e1f
Tyler Green <greent@tyleragreen.com>
2019-10-11 08:15:40 -0400

    Remove main

--- yard/server.py
+++ yard/server.py
@@ -10,9 +10,3 @@ app = ThirdRail()
 @app.consist('/ping')
 def get_ping():
     return json.dumps({'status': 'ok'}), '200 OK'
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(message)s')
-    app.start(port=5050)
