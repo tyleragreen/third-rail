1be4c8451c41ea37b9d6c942c079d6e304e2e2e3
Tyler Green <greent@tyleragreen.com>
2019-10-13 16:19:53 -0400

    Rename consist to route

--- example/server.py
+++ example/server.py
@@ -7,6 +7,6 @@ from thirdrail import ThirdRail
 app = ThirdRail()
 
 
-@app.consist('/ping')
+@app.route('/ping')
 def get_ping():
     return json.dumps({'status': 'ok'}), '200 OK'
--- thirdrail/server.py
+++ thirdrail/server.py
@@ -1,31 +1,27 @@
 import socket
-import logging
 from functools import wraps
-
-
-logger = logging.getLogger(__name__)
 
 
 class ThirdRail:
     def __init__(self):
         self.routes = {}
 
-    def consist(self, route):
+    def route(self, route):
         @wraps(route)
         def decorated(*args, **kwargs):
             self.routes[route] = args[0]
         return decorated
 
-    # The __call__ method implements WSGI described in PEP-333.
+    # The __call__ method implements the WSGI application interface
+    # described in PEP 333.
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
         if path in self.routes:
             body, status = self.routes[path]()
         else:
-            status = '404 Not Found'
-            body = ''
+            status, body = '404 Not Found', ''
 
-        response_headers = [('Content-type', 'text/plain')]
+        response_headers = [('Content-Type', 'application/json')]
         start_response(status, response_headers)
 
         if not isinstance(body, bytes):
